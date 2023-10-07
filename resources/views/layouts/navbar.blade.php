49784511cc9810e4c977899033a16e55bcc782d9
Clara Condori <condorihclara62@gmsil.com>
2023-10-06 23:36:28 -0300

    lo estoy logrando inge

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="/">Añadir nuevo</a>
+                    <a class="nav-link" href="/home">Pagina Principal</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('product.index') }}">Volver</a>
