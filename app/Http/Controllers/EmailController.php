5fcc551454d20709340e03299f22cc149612358f
Clara Condori <condorihclara62@gmsil.com>
2023-11-19 13:14:37 -0300

    formulario de correos

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -11,14 +11,14 @@ class EmailController extends Controller
         $asunto = $request->input('asunto');
         $email = $request->input('email');
         $mensaje = $request->input('mensaje');
-        $para = 'Nosotros@gmail.com';
+        $para = 'condorihclara62@gmail.com';
 
         Mail::send('correo.email', $request->all(), function($msg) use($asunto,$nombre,$email,$para){
             $msg->from($email,$nombre);
             $msg->subject($asunto);
             $msg->to($para);
         });
-        return view('welcome');
+        return view('home');
     }
 
     //
